7a420e041f49988257b2fa6e1fa1060810e2bcdc
nianhui.guo <Nianhui.Guo@HPI.de>
2022-12-05 15:13:38 +0100

    rename the script

---
+++ src/script/BNext-Large/run_distributed_on_disk_a6k5_AdamW_Curicullum_Large_assistant_teacher_num_3_aa.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+clear
+mkdir log 
+python3 train_assistant_group_amp.py --model bnext_large --distillation False --teacher_num 4 --assistant_teacher_num 3 --weak_teacher EfficientNet_B0 --mixup 0.0 --cutmix 0.0 --aug-repeats 1  --dali_cpu  --multiprocessing-distributed --dist-url 'tcp://127.0.0.1:33483' --dist-backend 'nccl' --world-size 1 --rank 0 --data=/data/imagenet/  --batch_size 128 --learning_rate=1e-3  --epochs=512 --weight_decay=0 | tee -a log/training.txt
+
+
